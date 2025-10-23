c8b0e7c51414a184e5d842133d3b8d3618985760
moha4733 <moonyahidi@gmail.com>
2025-10-23 10:14:19 +0200

    tiføjet nye klasser

---
+++ src/main/java/com/example/miniprojekt/Controller/UserController.java
@@ -0,0 +1,17 @@
+package com.example.miniprojekt.Controller;
+
+import com.example.miniprojekt.Service.UserService;
+import com.example.miniprojekt.Service.WishItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping()
+
+}
---
+++ src/main/java/com/example/miniprojekt/Repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.miniprojekt.Repository;
+
+public class UserRepository {
+
+}
