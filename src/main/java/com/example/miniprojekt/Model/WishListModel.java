b1189529445f9f4a3273a5586f864f3f0d1a1851
Aden0020 <aden0020@hotmail.com>
2025-10-27 13:59:20 +0100

    added change

--- src/main/java/com/example/miniprojekt/Model/WishListModel.java
+++ src/main/java/com/example/miniprojekt/Model/WishListModel.java
@@ -2,9 +2,48 @@ package com.example.miniprojekt.Model;
 
 import java.util.List;
 
-//public class WishListModel {
-//    private int wishListID;
-//    private String wishListItem;
-//    private String wishListName;
-//    private List<WishItem> wishItemList;
-//}
+public class WishListModel {
+    private int wishListID;
+    private String wishListItem;
+    private String wishListName;
+    private List<WishItemModel> wishItemList;
+
+
+    public WishListModel() {
+    }
+
+    public int getWishListID() {
+        return wishListID;
+    }
+
+    public void setWishListID(int wishListID) {
+        this.wishListID = wishListID;
+    }
+
+    public String getWishListItem() {
+        return wishListItem;
+    }
+
+    public void setWishListItem(String wishListItem) {
+        this.wishListItem = wishListItem;
+    }
+
+    public String getWishListName() {
+        return wishListName;
+    }
+
+    public void setWishListName(String wishListName) {
+        this.wishListName = wishListName;
+    }
+
+    public List<WishItemModel> getWishItemList() {
+        return wishItemList;
+    }
+
+    public void setWishItemList(List<WishItemModel> wishItemList) {
+        this.wishItemList = wishItemList;
+    }
+}
+
+
+
--- src/main/java/com/example/miniprojekt/Service/ListService.java
+++ src/main/java/com/example/miniprojekt/Service/ListService.java
@@ -1,8 +1,28 @@
 package com.example.miniprojekt.Service;
 
 import com.example.miniprojekt.Repository.WishItemRepository;
+import com.example.miniprojekt.Repository.WishListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
-//public class ListService {
+public class ListService {
+
+    @Autowired
+    private WishListRepository wishListRepository;
+
+
+    public ListService(WishListRepository wishListRepository) {
+        this.wishListRepository = wishListRepository;
+    }
+}
+
+
+
+
+
+
+
+
+
 //    public void deleteItemTitle(String itemTitle) {
 //        WishItemRepository.deleteItemTitle(itemTitle);
 //    }
