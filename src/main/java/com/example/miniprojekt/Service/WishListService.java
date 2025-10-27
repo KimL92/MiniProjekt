e52cf373d78372136225aae6052e790918c2b8e7
Aden0020 <aden0020@hotmail.com>
2025-10-27 14:03:38 +0100

    added change

--- src/main/java/com/example/miniprojekt/Service/WishListService.java
+++ src/main/java/com/example/miniprojekt/Service/WishListService.java
@@ -1,16 +1,15 @@
 package com.example.miniprojekt.Service;
 
-import com.example.miniprojekt.Repository.WishItemRepository;
 import com.example.miniprojekt.Repository.WishListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class ListService {
+public class WishListService {
 
     @Autowired
     private WishListRepository wishListRepository;
 
 
-    public ListService(WishListRepository wishListRepository) {
+    public WishListService(WishListRepository wishListRepository) {
         this.wishListRepository = wishListRepository;
     }
 }
